74dec03d44fbf8a17e77fc535fb10a151bb63014
Andrew Lim <andrewvincent319@gmail.com>
2020-07-23 12:40:12 -0500

    Add metrics reducer to root reducers. Create the Metrics component that fetches for metrics and returns the select component

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import { Provider, createClient } from 'urql';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import Metrics from '../Features/Metrics/Metrics';
 
 const useStyles = makeStyles(theme => ({
   header: {
     marginTop: '50px',
   },
 }));
+
+const client = createClient({
+  url: 'https://react.eogresources.com/graphql',
+});
 
 export default () => {
   const classes = useStyles();
@@ -16,6 +22,11 @@ export default () => {
     <Grid container direction="column" justify="center" alignItems="center">
       <Grid className={classes.header} item>
         <Typography variant="h4">Dashboard</Typography>
+      </Grid>
+      <Grid className={classes.header} item>
+        <Provider value={client}>
+          <Metrics />
+        </Provider>
       </Grid>
     </Grid>
   );
